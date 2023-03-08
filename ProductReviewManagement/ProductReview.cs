e983e9da6968cb1d192efb3292f6fbd60471847e
Raojyoti <117572975+RaoJyoti123@users.noreply.github.com>
2023-03-08 19:48:21 +0530

    [Jyoti Rao] Create ProductionReview class and all list in main method

---
+++ ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    /// <summary>
+    /// UC-1 Create ProductReview class
+    /// </summary>
+    public class ProductReview
+    {
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public bool IsLike { get; set; }
+    }
+}
